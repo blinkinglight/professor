6b1c0ef2cbba102b32c4907342af049f94b6b829
Mindaugas Žvirblis <tinklapiai@gmail.com>
2018-09-24 10:27:55 +0300

    security token

--- pprof.go
+++ pprof.go
@@ -6,18 +6,59 @@ import (
 	"net/http/pprof"
 )
 
+var token = "securitytoken"
+
 // init disables default handlers registered by importing net/http/pprof.
 func init() {
 	http.DefaultServeMux = http.NewServeMux()
 }
 
+func SetToken(t string) {
+	token = t
+}
+
 // Handle adds standard pprof handlers to mux.
 func Handle(mux *http.ServeMux) {
-	mux.HandleFunc("/debug/pprof/", pprof.Index)
-	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	mux.HandleFunc("/debug/pprof/", handleIndex)
+	mux.HandleFunc("/debug/pprof/cmdline", handleCmdline)
+	mux.HandleFunc("/debug/pprof/profile", handleProfile)
+	mux.HandleFunc("/debug/pprof/symbol", handleSymbol)
+	mux.HandleFunc("/debug/pprof/trace", handleTrace)
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	if !checkToken(w, r) {
+		return
+	}
+	pprof.Index(w, r)
+}
+
+func handleCmdline(w http.ResponseWriter, r *http.Request) {
+	if !checkToken(w, r) {
+		return
+	}
+	pprof.Cmdline(w, r)
+}
+
+func handleProfile(w http.ResponseWriter, r *http.Request) {
+	if !checkToken(w, r) {
+		return
+	}
+	pprof.Profile(w, r)
+}
+
+func handleSymbol(w http.ResponseWriter, r *http.Request) {
+	if !checkToken(w, r) {
+		return
+	}
+	pprof.Symbol(w, r)
+}
+
+func handleTrace(w http.ResponseWriter, r *http.Request) {
+	if !checkToken(w, r) {
+		return
+	}
+	pprof.Symbol(w, r)
 }
 
 // NewServeMux builds a ServeMux and populates it with standard pprof handlers.
@@ -43,6 +84,14 @@ func ListenAndServe(addr string) error {
 // Launch a standard pprof server at addr.
 func Launch(addr string) {
 	go func() {
-		log.Fatal(ListenAndServe(addr))
+		log.Println(ListenAndServe(addr))
 	}()
+}
+
+func checkToken(w http.ResponseWriter, r *http.Request) bool {
+	if r.URL.Query().Get("token") != token {
+		http.NotFound(w, r)
+		return false
+	}
+	return true
 }
